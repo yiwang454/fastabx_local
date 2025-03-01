d10815f27f67cb24e9dc611856a9068e620066ae
mxmpl <44207491+mxmpl@users.noreply.github.com>
2025-03-01 22:48:03 +0100

    refactor load_dtw_extension and correctly handle RMM logs

--- src/fastabx/__init__.py
+++ src/fastabx/__init__.py
@@ -1,13 +1,15 @@
+# ruff: noqa: E402
 """Full ABX."""
+
+from fastabx.utils import load_dtw_extension
+
+load_dtw_extension()
 
 from fastabx.dataset import Dataset
 from fastabx.pooling import pooling
 from fastabx.score import Score
 from fastabx.subsample import Subsampler
 from fastabx.task import Task
-from fastabx.utils import Environment
 from fastabx.zerospeech import zerospeech_abx
-
-Environment.load_dtw_extension()
 
 __all__ = ["Dataset", "Score", "Subsampler", "Task", "pooling", "zerospeech_abx"]
--- src/fastabx/utils.py
+++ src/fastabx/utils.py
@@ -1,5 +1,6 @@
 """Various utilities."""
 
+import os
 import sys
 from dataclasses import dataclass, field
 from typing import Literal
@@ -10,8 +11,9 @@ import torch
 
 def default_engine() -> Literal["cpu", "gpu"]:
     """Engine is 'gpu' if available, else 'cpu'."""
+    os.environ["RMM_DEBUG_LOG_FILE"] = os.devnull  # Avoid creation of rmm_log.txt logs by RAPIDS Memory Manager
     try:
-        pl.LazyFrame({"x": 1}).collect(engine="gpu")
+        pl.LazyFrame().collect(engine="gpu")
     except (ModuleNotFoundError, pl.exceptions.ComputeError):
         return "cpu"
     else:
@@ -25,18 +27,18 @@ class Environment:
     device: torch.device = field(default_factory=lambda: torch.device("cuda" if torch.cuda.is_available() else "cpu"))
     engine: Literal["cpu", "gpu"] = field(default_factory=default_engine)
 
-    @staticmethod
-    def load_dtw_extension() -> None:
-        """Load the DTW extension.
 
-        On Linux, we check that PyTorch has been installed with the correct CUDA version.
-        """
-        cuda_version = "12.4"
-        if sys.platform == "linux" and torch.version.cuda != cuda_version:
-            msg = (
-                f"On Linux, the DTW extension requires PyTorch with CUDA {cuda_version}. "
-                "It it not compatible with other CUDA versions, or with the CPU only version of PyTorch, "
-                "even if you wanted to only use the CPU backend of the DTW. "
-            )
-            raise ImportError(msg)
-        from . import _C  # type: ignore[attr-defined] # noqa: F401
+def load_dtw_extension() -> None:
+    """Load the DTW extension.
+
+    On Linux, we check that PyTorch has been installed with the correct CUDA version.
+    """
+    cuda_version = "12.4"
+    if sys.platform == "linux" and torch.version.cuda != cuda_version:
+        msg = (
+            f"On Linux, the DTW extension requires PyTorch with CUDA {cuda_version}. "
+            "It it not compatible with other CUDA versions, or with the CPU only version of PyTorch, "
+            "even if you wanted to only use the CPU backend of the DTW. "
+        )
+        raise ImportError(msg)
+    from . import _C  # type: ignore[attr-defined] # noqa: F401
