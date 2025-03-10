20afe83a64d2848cc4d78fcec436ffa26a530681
mxmpl <44207491+mxmpl@users.noreply.github.com>
2025-03-10 17:11:23 +0100

    defaults in CLI help

--- src/fastabx/__main__.py
+++ src/fastabx/__main__.py
@@ -1,6 +1,7 @@
 """Entry point for the ZeroSpeech ABX evaluation."""
 
 import argparse
+from argparse import ArgumentDefaultsHelpFormatter
 
 from fastabx.distance import available_distances
 from fastabx.zerospeech import zerospeech_abx
@@ -8,16 +9,16 @@ from fastabx.zerospeech import zerospeech_abx
 
 def main() -> None:
     """ZeroSpeech ABX evaluation."""
-    parser = argparse.ArgumentParser(description="ZeroSpeech ABX")
+    parser = argparse.ArgumentParser(description="ZeroSpeech ABX", formatter_class=ArgumentDefaultsHelpFormatter)
     parser.add_argument("item", help="Path to the item file")
     parser.add_argument("features", help="Path to the features directory")
     parser.add_argument("--frequency", type=int, default=50, help="Feature frequency (in Hz)")
-    parser.add_argument("--speaker", choices=["within", "across"], default="within")
-    parser.add_argument("--context", choices=["within", "any"], default="within")
-    parser.add_argument("--distance", choices=available_distances(), default="cosine")
+    parser.add_argument("--speaker", choices=["within", "across"], default="within", help="Speaker mode")
+    parser.add_argument("--context", choices=["within", "any"], default="within", help="Context mode")
+    parser.add_argument("--distance", choices=available_distances(), default="cosine", help="Distance")
     parser.add_argument("--max-size-group", type=int, default=10, help="Maximum size of a cell")
     parser.add_argument("--max-x-across", type=int, default=5, help="With 'across', maximum number of X given (A, B)")
-    parser.add_argument("--seed", type=int, default=0)
+    parser.add_argument("--seed", type=int, default=0, help="Random seed")
     args = parser.parse_args()
 
     score = zerospeech_abx(
