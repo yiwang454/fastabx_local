c8852577ca02624d150f4900f9a06dfe0863c8f8
mxmpl <44207491+mxmpl@users.noreply.github.com>
2025-03-16 20:27:55 +0100

    add openmp to windows and macos wheels

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 """Build the DTW PyTorch C++ extension."""
 
 import os
+import subprocess
 import sys
 from pathlib import Path
 
@@ -8,13 +9,30 @@ from setuptools import Extension, setup
 from torch.utils.cpp_extension import CUDA_HOME, BuildExtension, CppExtension, CUDAExtension
 
 
+def get_openmp_flags() -> tuple[list[str], list[str]]:
+    """Return the compiler and linker flags for OpenMP."""
+    match sys.platform:
+        case "linux":
+            compile_flags, link_flags = ["-fopenmp"], ["-fopenmp"]
+        case "win32":
+            compile_flags, link_flags = ["-openmp"], []
+        case "darwin":
+            brew = ["brew", "--prefix", "libomp"]
+            prefix = subprocess.run(brew, check=True, text=True, capture_output=True).stdout.strip()  # noqa: S603
+            compile_flags = ["-Xclang", "-fopenmp"]
+            link_flags = [f"-I{prefix}/include", f"-L{prefix}/lib", "-lomp", f"-Wl,-rpath,{prefix}/lib"]
+        case _:
+            return [], []
+    return compile_flags, link_flags
+
+
 def get_extension() -> Extension:
     """Either CUDA or CPU extension."""
     use_cuda = CUDA_HOME is not None
     extension = CUDAExtension if use_cuda else CppExtension
-    openmp = ["-fopenmp"] if sys.platform == "linux" else []
+    openmp_flags = get_openmp_flags()
     extra_compile_args = {
-        "cxx": ["-fdiagnostics-color=always", "-DPy_LIMITED_API=0x030c0000", "-O3", *openmp],
+        "cxx": ["-fdiagnostics-color=always", "-DPy_LIMITED_API=0x030C0000", "-O3"] + openmp_flags[0],
         "nvcc": ["-O3"],
     }
     sources = [Path("src/fastabx/csrc/dtw.cpp")]
@@ -25,7 +43,7 @@ def get_extension() -> Extension:
         "fastabx._C",
         sources,
         extra_compile_args=extra_compile_args,
-        extra_link_args=openmp,
+        extra_link_args=openmp_flags[1],
         py_limited_api=True,
     )
 
