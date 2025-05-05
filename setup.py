bb10d3488cb87ea35c90b1f98984a93e952101f8
mxmpl <44207491+mxmpl@users.noreply.github.com>
2025-05-05 11:04:54 +0200

    Revert changes to OpenMP flags for Linux and Windows

--- setup.py
+++ setup.py
@@ -1,25 +1,45 @@
 """Build the DTW PyTorch C++ extension."""
 
 import os
+import sys
 from pathlib import Path
 
 from setuptools import Extension, setup
 from torch.utils.cpp_extension import CUDA_HOME, BuildExtension, CppExtension, CUDAExtension
 
 
+def get_openmp_flags() -> tuple[list[str], list[str]]:
+    """Return the compiler and linker flags for OpenMP."""
+    match sys.platform:
+        case "linux":
+            compile_flags, link_flags = ["-fopenmp"], ["-fopenmp"]
+        case "win32":
+            compile_flags, link_flags = ["-openmp"], []
+        case _:  # On MacOS, we use the OpenMP version vendored by PyTorch
+            return [], []
+    return compile_flags, link_flags
+
+
 def get_extension() -> Extension:
     """Either CUDA or CPU extension."""
     use_cuda = CUDA_HOME is not None
     extension = CUDAExtension if use_cuda else CppExtension
+    openmp_flags = get_openmp_flags()
     extra_compile_args = {
-        "cxx": ["-fdiagnostics-color=always", "-DPy_LIMITED_API=0x030C0000", "-O3"],
+        "cxx": ["-fdiagnostics-color=always", "-DPy_LIMITED_API=0x030C0000", "-O3"] + openmp_flags[0],
         "nvcc": ["-O3"],
     }
     sources = [Path("src/fastabx/csrc/dtw.cpp")]
     if use_cuda:
         os.environ["TORCH_CUDA_ARCH_LIST"] = "Volta;Turing;Ampere;Ada;Hopper"
         sources.append(Path("src/fastabx/csrc/cuda/dtw.cu"))
-    return extension("fastabx._C", sources, extra_compile_args=extra_compile_args, py_limited_api=True)
+    return extension(
+        "fastabx._C",
+        sources,
+        extra_compile_args=extra_compile_args,
+        extra_link_args=openmp_flags[1],
+        py_limited_api=True,
+    )
 
 
 setup(
