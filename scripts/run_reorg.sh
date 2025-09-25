155b076436751d15964c54f8e51dbe43b0afe52d
yiwang454 <yiwang454@gmail.com>
2025-09-26 00:03:46 +0100

    prepare to get massive selected abx score2

--- scripts/run_reorg.sh
+++ scripts/run_reorg.sh
@@ -1,14 +1,9 @@
 for num in 100 200 300 400 600 900
 do 
-    python scripts/reorg_accenterror_df.py /home/s2522559/datastore/RepCodec_codecs/repcodec_hubert_large_l18_ls100_merge0.2vctk/VCTK_tokens_reorg_abx_errors_nolevel.csv number $num;
+    python scripts/reorg_accenterror_df.py /home/s2522559/datastore/RepCodec_codecs/repcodec_hubert_large_l18_ls100_merge0.5vctk/VCTK_tokens_reorg_abx_errors_nolevel.csv number $num;
 done
 
 for ratio in 0.025 0.05 0.1 0.2 
 do
-    python scripts/reorg_accenterror_df.py /home/s2522559/datastore/RepCodec_codecs/repcodec_hubert_large_l18_ls100_merge0.2vctk/VCTK_tokens_reorg_abx_errors_nolevel.csv ratio $ratio;
-done
-
-for thres in 0.1 0.2 0.3
-do
-    python scripts/reorg_accenterror_df.py /home/s2522559/datastore/RepCodec_codecs/repcodec_hubert_large_l18_ls100_merge0.2vctk/VCTK_tokens_reorg_abx_errors_nolevel.csv threshold $thres;
+    python scripts/reorg_accenterror_df.py /home/s2522559/datastore/RepCodec_codecs/repcodec_hubert_large_l18_ls100_merge0.5vctk/VCTK_tokens_reorg_abx_errors_nolevel.csv ratio $ratio;
 done
