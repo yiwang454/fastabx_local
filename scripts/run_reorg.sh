005ce92a29054fd8ad193f1fc4833257c65906ab
yiwang454 <yiwang454@gmail.com>
2025-09-25 22:47:00 +0100

    prepare to get massive selected abx score

--- scripts/run_reorg.sh
+++ scripts/run_reorg.sh
@@ -1,14 +1,14 @@
 for num in 100 200 300 400 600 900
 do 
-    python scripts/reorg_accenterror_df.py /home/s2522559/workspace/fastabx/scripts/results/abx_errors_nolevel.csv number $num;
+    python scripts/reorg_accenterror_df.py /home/s2522559/datastore/vctk/hubert_feature/large_l18_mic1_abx_errors_nolevel.csv number $num;
 done
 
 for ratio in 0.025 0.05 0.1 0.2 
 do
-    python scripts/reorg_accenterror_df.py /home/s2522559/workspace/fastabx/scripts/results/abx_errors_nolevel.csv ratio $ratio;
+    python scripts/reorg_accenterror_df.py /home/s2522559/datastore/vctk/hubert_feature/large_l18_mic1_abx_errors_nolevel.csv ratio $ratio;
 done
 
 for thres in 0.1 0.2 0.3
 do
-    python scripts/reorg_accenterror_df.py /home/s2522559/workspace/fastabx/scripts/results/abx_errors_nolevel.csv threshold $thres;
+    python scripts/reorg_accenterror_df.py /home/s2522559/datastore/vctk/hubert_feature/large_l18_mic1_abx_errors_nolevel.csv threshold $thres;
 done
--- scripts/run_vctk_codec_abx.sh
+++ scripts/run_vctk_codec_abx.sh
@@ -1,9 +1,6 @@
-for dir in '' '_merge0.2vctk' '_merge0.5vctk' '_merge1.0vctk' '_released'
+for dir in '_merge0.2vctk' '_merge0.5vctk' '_merge1.0vctk' '_released'
 do
-  python scripts/vctk_codec_abx.py /home/s2522559/datastore items_vctk/vctk_largeclass_debug_mannualaccent_test.item 18 'RepCodec_codecs/repcodec_hubert_large_l18_ls100'$dir'/VCTK_tokens_reorg' \
-	'RepCodec_codecs/repcodec_hubert_large_l18_ls100'$dir'/repcodec_hubert_large_l18_ls100'$dir'_codebook.pt' --abx_mode accent_abx --frame_mean >> 'scripts/results/repcodec_hubert_large_l18_ls100'$dir'_accent_framemean.txt'
-
-  python scripts/vctk_codec_abx.py /home/s2522559/datastore items_vctk/vctk_largeclass_debug_mannualaccent_test.item 18 'RepCodec_codecs/repcodec_hubert_large_l18_ls100'$dir'/VCTK_tokens_reorg' \
-	'RepCodec_codecs/repcodec_hubert_large_l18_ls100'$dir'/repcodec_hubert_large_l18_ls100'$dir'_codebook.pt' --abx_mode phone_abx --frame_mean >> 'scripts/results/repcodec_hubert_large_l18_ls100'$dir'_phone_framemean.txt'
+  python scripts/vctk_codec_abx.py /home/s2522559/datastore /home/s2522559/datastore/items_vctk/vctk_word_test.item 18 'RepCodec_codecs/repcodec_hubert_large_l18_ls100'$dir'/VCTK_tokens_reorg' \
+	--codebook_path 'RepCodec_codecs/repcodec_hubert_large_l18_ls100'$dir'/repcodec_hubert_large_l18_ls100'$dir'_codebook.pt' --abx_mode accent_word;
   # echo "scripts/results/repcodec_hubert_large_l18_ls100"$dir".txt"
 done
