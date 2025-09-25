e83120db34ed43750ab24c11fae748db391f0fac
yiwang454 <yiwang454@gmail.com>
2025-09-26 00:08:46 +0100

    prepare to get massive selected abx score4

--- scripts/run_reorg.sh
+++ scripts/run_reorg.sh
@@ -1,9 +1,9 @@
 for num in 100 200 300 400 600 900
 do 
-    python scripts/reorg_accenterror_df.py /home/s2522559/datastore/RepCodec_codecs/repcodec_hubert_large_l18_ls100_merge1.0vctk/VCTK_tokens_reorg_abx_errors_nolevel.csv number $num;
+    python scripts/reorg_accenterror_df.py /home/s2522559/datastore/RepCodec_codecs/repcodec_hubert_large_l18_ls100_released/VCTK_tokens_reorg_abx_errors_nolevel.csv number $num;
 done
 
 for ratio in 0.025 0.05 0.1 0.2 
 do
-    python scripts/reorg_accenterror_df.py /home/s2522559/datastore/RepCodec_codecs/repcodec_hubert_large_l18_ls100_merge1.0vctk/VCTK_tokens_reorg_abx_errors_nolevel.csv ratio $ratio;
+    python scripts/reorg_accenterror_df.py /home/s2522559/datastore/RepCodec_codecs/repcodec_hubert_large_l18_ls100_released/VCTK_tokens_reorg_abx_errors_nolevel.csv ratio $ratio;
 done
